6cf63d3b7e8bc71425716dd53303cb8079c80a0f
Steve Kane <kanesteven@gmail.com>
2017-02-27 15:23:40 -0600

    simplified construction and provided fall-through for invalid or unspecified dimension

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
 module.exports = BigTriangle
 
-function bound ( min, max, v ) {
-  return v < min
-    ? min
-    : v > max 
-      ? max 
-      : v
-}
-
 function BigTriangle ( dimensionality ) {
-  const MIN_DIMENSION = 2
-  const MAX_DIMENSION = 4
-  const dim = bound(MIN_DIMENSION, MAX_DIMENSION, dimensionality)
-  const Ctor = Float32Array != null ? Float32Array : Array
-  const out = new Ctor(3 * dim)
-
-  out[0] = -4
-  out[1] = -4
-  out[dim] = 0
-  out[dim+1] = 4
-  out[dim * 2] = 4
-  out[dim * 2 + 1] = -4
-
-  if ( dim === 4 ) {
-    out[dim * 0 + 3] = 1 
-    out[dim * 1 + 3] = 1 
-    out[dim * 2 + 3] = 1 
+  switch ( dimensionality ) {
+    case 4: return [ 
+      -4, -4, 0, 1,
+      0, 4, 0, 1,
+      4, -4, 0, 1
+    ]
+    case 3: return [
+      -4, -4, 0,
+      0, 4, 0,
+      4, -4, 0
+    ]
+    case 2:
+    default: return [
+      -4, -4,
+      0, 4,
+      4, -4
+    ]
   }
-  return out 
 }
--- test.js
+++ test.js
@@ -2,10 +2,16 @@ var test = require('tape')
 var BT = require('./index')
 
 test('construction', function (t) {
+  var btdefault = BT()
   var bt2d = BT(2)
   var bt3d = BT(3)
   var bt4d = BT(4)
 
+  t.same(btdefault, [
+    -4, -4, 
+    0, 4, 
+    4, -4 
+  ])
   t.same(bt2d, [ 
     -4, -4, 
     0, 4, 
